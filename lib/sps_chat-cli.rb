e7e96fa937915e6b89da188803d4b280209f14f7
Your Name <you@example.com>
2016-07-26 22:16:47 +0100

    feature: Interactive typing mode now implemented.

--- lib/sps_chat-cli.rb
+++ lib/sps_chat-cli.rb
@@ -13,46 +13,91 @@ BACKSPACE = "\u007F"
 class SPSChatCli < SPSChat
 
   def initialize(host: '127.0.0.1', port: '8080', \
-                  userid: 'user' + (0..1000).to_a.sample.to_s, room: '')
+                  userid: 'user' + (0..1000).to_a.sample.to_s, room: '', typing_mode: false)
 
     super(host: host, port: port, userid: userid, room: room)
-    terminate = false
+    
+    @userid, @typing_mode = userid, typing_mode
+    terminated = false
+    @user, @history = {}, []
+    
     sleep 1
     print "\r\n"
 
     loop do
 
-      s = ''
+      @s = ''
 
-      print "\r> "
+      print "\r#{userid}> "
 
       begin
 
         c = $stdin.getch
-        (terminate = true; break) if c == CTRLC
-
-        unless c == BACKSPACE then
-          s += c 
-          print c
-        else
-          s.chop!
-          print "\r>  " + ' ' * s.length
-          print "\r> " + s
+        (terminated = true; break) if c == CTRLC
+        
+        if typing_mode then
+          
+          unless c == "\r" then
+            typing c 
+          else
+            c = "\n"            
+            @history << "#{userid}> " + @s
+            send @s            
+          end
         end
-      end until c == "\r" or terminate
 
-      break if terminate
+        @s = savebuffer @s, c
+        print "\r#{@userid}>  " + ' ' * @s.length if c == BACKSPACE
+        print "\r#{@userid}> " + @s 
+        
+      end until c == "\r" or c == "\n" or terminated
 
-      send s
-      print "\r" + @userid + '> ' + s + "\n"
+      break if terminated
+      
+      unless typing_mode then
+        print "\r" + @userid + '> ' + @s + "\n"
+        send @s
+      end
+
     end 
   end
 
-  def onincoming(sender, msg)
-    s = ''
-    print "\r%s> %s\n" % [sender.strip, msg]
-    print "\r> "
+  def onincoming(sender, msg, typing_mode=false)    
+    
+    if @typing_mode then
+
+      if msg.length <= 1 then
+        c = msg                
+
+        @user[sender] ||= ''
+        @user[sender] = savebuffer @user[sender], c             
+        print "\e[H\e[2J"
+        print "\r\n" + @history.join("\r\n") + "\r\n"
+      else        
+        c = "\n"
+        @history << "#{sender}> " + @user[sender]
+        @user[sender] = ''
+      end
+ 
+      @user.each do |u, s|
+        print "\r\r#{u}> " + s  + "\n" unless s.empty?
+      end
+
+      print "\r#{@userid}> " + @s  
+      
+    else
+      print "\r%s> %s\n" % [sender, msg]
+      print "\r%s> " % @userid
+    end
   end
+  
+  def savebuffer(s, c='')
+    
+    c == BACKSPACE ?  s.chop! : s += c 
+    
+    return s
+  end
+  
+  
 
 end
-
